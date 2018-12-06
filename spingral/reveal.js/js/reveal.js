ffbcfee2721a42c364d3225b563682e28a370a18
alan <alanpboette@gmail.com>
2018-12-05 22:05:50 -0300

    add

--- spingral/reveal.js/js/reveal.js
+++ spingral/reveal.js/js/reveal.js
@@ -5110,3 +5110,12 @@
 	return Reveal;
 
 }));
+
+window.addEventListener("mousedown", handleClick, false);
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); }, false);
+
+function handleClick(e) {
+	e.preventDefault();
+	if(e.button === 0) Reveal.next(); 
+	if(e.button === 2) Reveal.prev(); 
+}
