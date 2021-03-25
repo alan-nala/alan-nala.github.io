129e2f2d8a3056d7d68c39bb8eee6391a81d7502
Your Name <youremail@domain.com>
2021-03-24 21:46:13 -0300

    Updates

---
+++ precache-manifest.55d634427acda6214d30678acf324760.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f01c98d919930253edbd20e6b86aa4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43d7578780785d6292e8",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "33fe5fbfc0af24700fdf",
+    "url": "/static/js/2.360b4329.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.360b4329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d7578780785d6292e8",
+    "url": "/static/js/main.b7148f92.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "2856e94a22b048eac3427978475306df",
+    "url": "/static/media/conicet.2856e94a.png"
+  },
+  {
+    "revision": "10239774131aff52462168be58b48047",
+    "url": "/static/media/foto-cuadrada.10239774.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "cf33f4c7495aa08d5334daf93d2d2966",
+    "url": "/static/media/resume.cf33f4c7.pdf"
+  },
+  {
+    "revision": "94203f1c0382cf978317b7924c0f15ba",
+    "url": "/static/media/unlp.94203f1c.png"
+  }
+]);
